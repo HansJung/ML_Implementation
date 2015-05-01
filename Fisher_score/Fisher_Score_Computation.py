a5da24cd4d9ad8e1ce2ee16d5dbc637efdca48b3
Hans Jung <yhansjung@gmail.com>
2015-05-01 14:47:18 +0900

    150501
    Fisher_score

---
+++ Fisher_score/Fisher_Score_Computation.py
@@ -0,0 +1,49 @@
+'''
+Compute the fisher score
+'''
+
+from itertools import combinations
+import numpy as np
+
+class Fisher_Score:
+    def __init__(self, Training):
+        '''
+        Training record
+        - Dict.key : label
+        - Dict.value : Each features. (n1 by p matrix)
+
+        Goal
+        - Find the distinguishable column p'
+        '''
+        self.Training = Training
+        self.List_Training_Key = Training.keys()
+
+    def Compute_each_pair(self):
+        All_Class_Fisher_Score = []
+        for KeySubset in combinations(self.List_Training_Key, 2):
+            # For each class
+            Mat_Training_A_T = np.matrix(self.Training[KeySubset[0]]).T
+            Mat_Training_B_T = np.matrix(self.Training[KeySubset[1]]).T
+
+            Each_Class_Fisher_score = []
+
+            for (Feature_A, Feature_B) in zip(Mat_Training_A_T, Mat_Training_B_T):
+                # Each Column is a feature.
+                Within_Var_each_feature = \
+                    np.var(Feature_A) + np.var(Feature_B)
+                Between_Var_each_feature = \
+                    np.abs(np.mean(Feature_A) - np.mean(Feature_B)) ** 2
+                Fisher_Score_each_feature = \
+                    Between_Var_each_feature / Within_Var_each_feature
+                Each_Class_Fisher_score.append(Fisher_Score_each_feature)
+            All_Class_Fisher_Score.append(Each_Class_Fisher_score)
+        return All_Class_Fisher_Score
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print None
